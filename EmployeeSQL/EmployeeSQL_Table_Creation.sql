76048060c83b853585cf33a01572e2940eb15d04
Carmela Monis <carmelamonis@gmail.com>
2021-05-08 22:14:37 -0700

    Adjust table creation, create queries, add ERD

--- EmployeeSQL/EmployeeSQL_Table_Creation.sql
+++ EmployeeSQL/EmployeeSQL_Table_Creation.sql
@@ -1,43 +1,65 @@
+--Drop Tables
+DROP TABLE departments CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE salaries CASCADE;
+DROP TABLE titles CASCADE;
+
 --Create tables
+
+CREATE TABLE titles
+(
+	title_id VARCHAR(30) NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	PRIMARY KEY(title_id)
+);
+
+CREATE TABLE employees
+(
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(30) NOT NULL,
+	birth_date VARCHAR(30),
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(30),
+	hire_date VARCHAR(30),
+	PRIMARY KEY(emp_no),
+	FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)
+	
+);
+
+CREATE TABLE salaries
+(
+	emp_no INT,
+	salary INT,
+	PRIMARY KEY(emp_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+
 
 CREATE TABLE departments
 (
 	dept_no VARCHAR(30) NOT NULL,
-	dept_name VARCHAR(30) NOT NULL
+	dept_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp
 (
-	emp_no INT,
-	dept_no VARCHAR(30) NOT NULL
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 	
 );
 
 CREATE TABLE dept_manager
 (
 	dept_no VARCHAR(30) NOT NULL,
-	emp_no INT
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
-CREATE TABLE employees
-(
-	emp_no INT,
-	emp_title_id VARCHAR(30) NOT NULL,
-	birth_date VARCHAR(30) NOT NULL,
-	first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-	sex VARCHAR(30) NOT NULL,
-	hire_date VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE salaries
-(
-	emp_no INT,
-	salary INT
-);
-
-CREATE TABLE titles
-(
-	title_id VARCHAR(30) NOT NULL,
-	title VARCHAR(30) NOT NULL
-);
---
+++ EmployeeSQL/QuickDBD-export.sql
@@ -0,0 +1,87 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [employees] (
+    [emp_no] INT  NOT NULL ,
+    [emp_title_id] VARCHAR(30)  NOT NULL ,
+    [birth_date] VARCHAR(30)  NOT NULL ,
+    [first_name] VARCHAR(100)  NOT NULL ,
+    [last_name] VARCHAR(100)  NOT NULL ,
+    [sex] VARCHAR(1)  NOT NULL ,
+    [hire_date] VARCHAR(30)  NOT NULL ,
+    CONSTRAINT [PK_employees] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [titles] (
+    [title_id] VARCHAR(30)  NOT NULL ,
+    [title] VARCHAR(100)  NOT NULL ,
+    CONSTRAINT [PK_titles] PRIMARY KEY CLUSTERED (
+        [title_id] ASC
+    )
+)
+
+CREATE TABLE [salaries] (
+    [emp_no] INT  NOT NULL ,
+    [salary] INT  NOT NULL ,
+    CONSTRAINT [PK_salaries] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [departments] (
+    [dept_no] VARCHAR(30)  NOT NULL ,
+    [dept_name] VARCHAR(30)  NOT NULL ,
+    CONSTRAINT [PK_departments] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC
+    )
+)
+
+CREATE TABLE [dept_emp] (
+    [emp_no] INT  NOT NULL ,
+    [dept_no] VARCHAR(30)  NOT NULL ,
+    CONSTRAINT [PK_dept_emp] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [dept_manager] (
+    [dept_no] VARCHAR(30)  NOT NULL ,
+    [emp_no] INT  NOT NULL ,
+    CONSTRAINT [PK_dept_manager] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC
+    )
+)
+
+ALTER TABLE [titles] WITH CHECK ADD CONSTRAINT [FK_titles_title_id] FOREIGN KEY([title_id])
+REFERENCES [employees] ([emp_title_id])
+
+ALTER TABLE [titles] CHECK CONSTRAINT [FK_titles_title_id]
+
+ALTER TABLE [salaries] WITH CHECK ADD CONSTRAINT [FK_salaries_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [salaries] CHECK CONSTRAINT [FK_salaries_emp_no]
+
+ALTER TABLE [departments] WITH CHECK ADD CONSTRAINT [FK_departments_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [dept_emp] ([dept_no])
+
+ALTER TABLE [departments] CHECK CONSTRAINT [FK_departments_dept_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_emp_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [departments] ([dept_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_dept_no]
+
+COMMIT TRANSACTION QUICKDBD
